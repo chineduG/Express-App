011a5cee4be20f05e139287b41b87cd87717362f
ChineduG <investorgbriel@gmail.com>
2023-03-26 14:59:53 +0100

    Database connection setup

--- database/conn.js
+++
@@ -1,7 +0,0 @@
-
-
-import mongoose from "mongoose";
-
-export default async function connect(){
-    mongoose.connect()
-}
--- index.js
+++ index.js
@@ -8,8 +8,10 @@ import cors from 'cors';
 import {config} from 'dotenv';
 import router from './router/route.js';
 
-const app = express();
+/** import connection file */
+import connect from './database/connect.js';
 
+const app = express();
 
 /** app middleware */
 app.use(morgan('tiny'));
@@ -20,11 +22,8 @@ config();
 /** applciation port */
 const PORT = process.env.PORT || 8080;
 
-
 /** routes */
 app.use('/api', router); /** api/v1*/
-
-
 
 app.get('/', (req,  res) => {
     try{
@@ -34,7 +33,17 @@ app.get('/', (req,  res) => {
     }
 })
 
-
-app.listen(PORT, () => {
-    console.log(`Server listening to http://localhost:${PORT}`);
+//! Database Connection
+/** start server only when we have valid connections with the Database */
+connect().then(() => {
+    try{
+        app.listen(PORT, () => {
+            console.log(`Server listening to http://localhost:${PORT}`);
+        })
+    }catch(error){
+        console.log('Cannot connect to the server');
+    }
+}).catch(error => {
+    console.log('Invalid Database Connection');
 })
+
