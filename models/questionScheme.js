d2c79b0311fc79e11c63e4d86d6ccf23546032fc
ChineduG <investorgbriel@gmail.com>
2023-03-26 15:29:46 +0100

    Models and Schema created

---
+++ models/questionScheme.js
@@ -0,0 +1,14 @@
+
+
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+/** question model */
+
+const questionModel = new Schema({
+    questions: {type: Array, default:[]},
+    answers:  {type: Array, default:[]},
+    createdAt: {type: Date, default: Date.now}
+});
+
+export const Questions = mongoose.model('Questions', questionModel);
